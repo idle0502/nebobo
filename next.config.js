3e721079bf80fc84c4a121e2042c2996ccd7a537
idle0502 <idleogosa@gmail.com>
2025-07-22 07:55:28 +0900

    🛠 fix: Webpack node scheme fallback 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        child_process: false,
+        fs: false,
+        os: false,
+        crypto: false,
+        path: false,
+        stream: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
