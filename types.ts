f10303a276102524d8087c61c66f9124095a505d
idle0502 <idleogosa@gmail.com>
2025-07-22 10:06:22 +0900

    수정1

---
+++ types.ts
@@ -0,0 +1,11 @@
+// types.ts
+export interface CardData {
+  link: string;
+  category: string;
+  member: string;
+  date: string;
+  duration: string;
+  thumbnail: string;
+  alt: string;
+  title: string;
+}
